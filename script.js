0debb49b3ef2d7d782cc6b8db61960a7f2daa21a
Pham99 <tomas.phamt@gmail.com>
2025-11-16 00:14:58 +0100

    80% various fixes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const mainMenuText = document.querySelectorAll('.text');
-const mainMenuTextArray = Array.from(mainMenuText);
-const img = document.querySelectorAll('img');
-
+// dark mode
 const darkModeToggle = document.getElementById('dark_mode_toggle');
 const bg = document.querySelector('.background');
+const img = document.querySelectorAll('img');
 if (darkModeToggle) {
     darkModeToggle.addEventListener('click', () => {
         bg.classList.toggle('background_dark');
@@ -15,6 +13,7 @@ if (darkModeToggle) {
     });
 }
 
+// tech item shine 
 const techItems = document.querySelectorAll('.tech_item_shine');
 techItems.forEach(item => {
     item.addEventListener('mouseenter', () => {
@@ -27,10 +26,10 @@ techItems.forEach(item => {
     });
 });
 
-const textContainersArray = Array.from(document.querySelectorAll('.text-container'));
+const textArray = Array.from(document.querySelectorAll('.text'));
 const scene3D = document.getElementsByClassName('scene3D')[0];
 const topMenu = document.getElementsByClassName('top_menu')[0];
-const topMenuItemsArray = Array.from(document.querySelectorAll('.top_menu_container'));
+const topMenuItemsArray = Array.from(document.querySelectorAll('.top_menu_item'));
 
 function removeSlideAnimation(el) {
     el.currentTarget.classList.remove('slideSide', 'slideUp', 'slideDown', 'slideSideReverse', 'slideUpReverse', 'slideDownReverse');
@@ -42,37 +41,44 @@ function removePointerDisable(el) {
     el.currentTarget.classList.remove('disabled');
 }
 
-textContainersArray.forEach(e => {
+textArray.forEach(e => {
     e.addEventListener('animationend', removeSlideAnimation);
     e.addEventListener('animationend', removePointerDisable);
 });
 
 function reveal(ev) {
-    const text = ev.currentTarget.querySelector('[data-target]');
-    const target = text.getAttribute('data-target');
+    if (!ev) return;
+    const target = ev.getAttribute('data-target');
     document.getElementById(target).classList.remove('display-none');
     document.getElementById(target).classList.add('animation_fade_in');
 }
 
 const title = document.querySelector('.title-text');
+const mainMenuTextArray = Array.from(document.querySelectorAll('.text'));
 
 mainMenuTextArray.forEach((el, idx) => {
     el.addEventListener('click', () => {
         scene3D.addEventListener('animationend', addDisplayNone, { once: true });
-        textContainersArray.forEach(e => {
+        textArray.forEach(e => {
             e.classList.add('disabled');
         });
-        reveal({ currentTarget: textContainersArray[idx] });
+        reveal(textArray[idx]);
         for (let i = 0; i < idx; i++) {
-            textContainersArray[i].classList.add('slideUp');
+            textArray[i].classList.add('slideUp');
         }
-        textContainersArray[idx].classList.add('slideSide');
-        for (let i = idx + 1; i < textContainersArray.length; i++) {
-            textContainersArray[i].classList.add('slideDown');
+        textArray[idx].classList.add('slideSide');
+        for (let i = idx + 1; i < textArray.length; i++) {
+            textArray[i].classList.add('slideDown');
         }
+        topMenuItemsArray.forEach(highlight => {
+            highlight.classList.remove('active');
+        });
+        idxy = idx + 1;
+        topMenuItemsArray[idxy].classList.add('active');
         topMenu.classList.remove('display-none');
-        topMenu.classList.add('animation_fade_in');
+        //topMenu.classList.add('animation_fade_in');
         title.classList.add('faded');
+        topMenuItemsArray[0].classList.remove('disabled');
     });
 });
 
@@ -85,7 +91,7 @@ const reverseSlideAnimations = [
 const mainContentArray = Array.from(document.querySelectorAll('.main_container > div'));
 function toggleMenu() {
     scene3D.classList.remove('display-none');
-    textContainersArray.forEach(e => {
+    textArray.forEach(e => {
         e.classList.add('disabled');
         // randomly choose an animation from reverseSlideAnimations and add it to the element
         const randomAnim = reverseSlideAnimations[Math.floor(Math.random() * reverseSlideAnimations.length)];
@@ -107,6 +113,7 @@ mainContentArray.forEach(section => {
 topMenuItemsArray.forEach((el, idx) => {
     if (idx === 0){
         el.addEventListener('click', () => {
+            el.classList.add('disabled');
             toggleMenu();
             title.classList.remove('faded');
             topMenu.classList.add('animation_fade_out');
@@ -119,25 +126,15 @@ topMenuItemsArray.forEach((el, idx) => {
         });
     }
     else{
-        el.addEventListener('click', (e) => {
+        el.addEventListener('click', () => {
             mainContentArray.forEach(section => {
                 section.classList.add('display-none');
             });
-            reveal(e);
+            topMenuItemsArray.forEach(highlight => {
+                highlight.classList.remove('active');
+            });
+            topMenuItemsArray[idx].classList.add('active');
+            reveal(el);
         });
-    }
-});
-
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'm' || e.key === 'M') {
-        toggleMenu();
-    }
-});
-
-const about_section = document.getElementById('about_section');
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'a' || e.key === 'A') {
-        about_section.classList.remove('display-none');
     }
 });

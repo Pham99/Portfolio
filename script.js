c284ea66aad45384593269bc8efecaeb456d34bf
Pham99 <tomas.phamt@gmail.com>
2025-12-31 01:01:13 +0100

    javascript overhaul

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 // dark mode
-const darkModeToggle = document.getElementById("dark-mode-toggle");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const darkModeToggle2 = document.querySelector("#dark-mode-toggle-2");
 const bg = document.querySelector(".background");
 const img = document.querySelectorAll("img");
-if (darkModeToggle) {
-	darkModeToggle.addEventListener("click", () => {
-		bg.classList.toggle("background-dark");
-		darkModeToggle.children[0].classList.toggle("fa-moon");
-		darkModeToggle.children[0].classList.toggle("fa-sun");
-		img.forEach((image) => {
-			image.classList.toggle("invert");
-		});
+
+function toggleDarkMode() {
+	bg.classList.toggle("background-dark");
+	const darkModeToggleIcon = darkModeToggle.querySelector("i");
+	darkModeToggleIcon.classList.toggle("fa-moon");
+	darkModeToggleIcon.classList.toggle("fa-sun");
+	if (darkModeToggle2.textContent === "Light Mode") {
+		darkModeToggle2.textContent = "Dark Mode";
+	}
+	else {
+		darkModeToggle2.textContent = "Light Mode";
+	}
+	img.forEach((image) => {
+		image.classList.toggle("invert");
 	});
 }
+darkModeToggle.addEventListener("pointerdown", () => toggleDarkMode());
+darkModeToggle2.addEventListener("pointerdown", () => toggleDarkMode());
 
 // tech item shine
 const techItems = document.querySelectorAll(".tech-item");
 techItems.forEach((item) => {
-	const shine = item.getElementsByClassName("tech-item-shine")[0];
+	const shine = item.querySelector(".tech-item-shine");
 	item.addEventListener("pointerenter", () => {
 		if (!shine.classList.contains("shine-animation")) {
 			shine.classList.add("shine-animation");
@@ -27,160 +36,193 @@ techItems.forEach((item) => {
 	});
 });
 
-const mainMenuItemsArray = Array.from(
-	document.querySelectorAll(".main-menu-item"),
-);
-const scene3D = document.getElementsByClassName("scene3D")[0];
-const navMenu = document.getElementsByClassName("nav-menu")[0];
-const navMenuItemsArray = Array.from(
-	document.querySelectorAll(".nav-menu-item"),
-);
+const MenuSystem = {
+	items: document.querySelectorAll(".main-menu-item"),
+	selectedItem: 0,
+	scene: document.querySelector(".scene3D"),
+	reverseAnimations: [
+		"slideSideReverse",
+		"slideUpReverse",
+		"slideDownReverse",
+	],
 
-function removePointerDisable(el) {
-	el.currentTarget.classList.remove("disabled");
-}
+	disableAllItems() {
+		this.items.forEach((e) => e.classList.add("disabled"));
+	},
 
-mainMenuItemsArray.forEach((e) => {
-	e.addEventListener("animationend", (e) => {
-		e.currentTarget.classList.remove(
-			"slideSide",
-			"slideUp",
-			"slideDown",
-			"slideSideReverse",
-			"slideUpReverse",
-			"slideDownReverse",
-		);
-	});
-	e.addEventListener("animationend", (e) => {
-	  e.currentTarget.classList.remove("disabled");
-	});
-});
-
-function reveal(ev) {
-	if (!ev) return;
-	const target = ev.getAttribute("data-target");
-	document.getElementById(target).classList.remove("display-none");
-	document.getElementById(target).classList.add("animation-fade-in");
-}
-
-const title = document.querySelector(".title-text");
-const mainMenuTextArray = Array.from(
-	document.querySelectorAll(".main-menu-item"),
-);
-
-mainMenuTextArray.forEach((el, idx) => {
-	el.addEventListener("click", () => {
-		scene3D.addEventListener("animationend", (el) => {
-		  el.currentTarget.classList.add("display-none");
-		}, { once: true });
-		mainMenuItemsArray.forEach((e) => {
-			e.classList.add("disabled");
-		});
-		reveal(mainMenuItemsArray[idx]);
-		for (let i = 0; i < idx; i++) {
-			mainMenuItemsArray[i].classList.add("slideUp");
+	hideMenu(id) {
+		for (let i = 0; i < id; i++) {
+			this.items[i].classList.add("slideUp");
 		}
-		mainMenuItemsArray[idx].classList.add("slideSide");
-		for (let i = idx + 1; i < mainMenuItemsArray.length; i++) {
-			mainMenuItemsArray[i].classList.add("slideDown");
+		this.items[id].classList.add("slideSide");
+		for (let i = id + 1; i < this.items.length; i++) {
+			this.items[i].classList.add("slideDown");
 		}
-		let idxy = idx + 1;
-		navMenuItemsArray.forEach((e) => {
-			e.classList.remove("active");
-		  e.classList.remove("disabled");
-		});
-		navMenuItemsArray[idxy].classList.add("active");
-		navMenu.classList.remove("display-none");
-		title.classList.add("faded");
-		hamburger_button.classList.add("expanded");
-	});
-});
+		this.selectedItem = id;
+	},
 
-const reverseSlideAnimations = [
-	"slideSideReverse",
-	"slideUpReverse",
-	"slideDownReverse",
-];
-
-const mainContentArray = Array.from(
-	document.querySelectorAll(".main-container > div"),
-);
-function toggleMenu() {
-	scene3D.classList.remove("display-none");
-	mainMenuItemsArray.forEach((e) => {
-		e.classList.add("disabled");
-		// randomly choose an animation from reverseSlideAnimations and add it to the element
-		const randomAnim =
-			reverseSlideAnimations[
-				Math.floor(Math.random() * reverseSlideAnimations.length)
-			];
-		e.classList.add(randomAnim);
-	});
-}
-
-navMenu.addEventListener("animationend", () => {
-	navMenu.classList.remove("animation-fade-in");
-	navMenu.classList.remove("animation-fade-out");
-});
-
-mainContentArray.forEach((section) => {
-	section.addEventListener("animationend", () => {
-		section.classList.remove("animation-fade-in");
-	});
-});
-
-const overlay = document.querySelector(".nav-menu-overlay");
-overlay.addEventListener("click", closeSideMenu);
-
-function closeSideMenu() {
-	overlay.classList.remove("nav-menu-overlay-active");
-	navMenu.classList.add("nav-menu-hidden");
-}
-
-function openSideMenu() {
-	overlay.classList.add("nav-menu-overlay-active");
-	navMenu.classList.remove("nav-menu-hidden");
-}
-
-const hamburger_button = document.querySelector(".hamburger-button");
-hamburger_button.addEventListener("click", () => {
-	openSideMenu();
-});
-
-navMenuItemsArray.forEach((el, idx) => {
-	if (idx === 0) {
-		el.addEventListener("click", () => {
-			navMenuItemsArray.forEach((e) => {
-				e.classList.add("disabled");
-			});
-			toggleMenu();
-			title.classList.remove("faded");
-			closeSideMenu();
-			hamburger_button.classList.remove("expanded");
-			navMenu.classList.add("animation-fade-out");
-			navMenu.addEventListener(
-				"animationend",
-				() => {
-					navMenu.classList.add("display-none");
-				},
-				{ once: true },
+	showMenu() {
+		this.scene.classList.remove("display-none");
+		this.items.forEach((e) => {
+			e.classList.remove(
+				"slideSide",
+				"slideUp",
+				"slideDown",
+				"slideSideReverse",
+				"slideUpReverse",
+				"slideDownReverse",
+				"disabled",
 			);
-			mainContentArray.forEach((section) => {
-				section.classList.add("display-none");
+			e.classList.remove("display-none");
+			e.classList.add("disabled");
+			let id = this.selectedItem;
+			for (let i = 0; i < id; i++) {
+				this.items[i].classList.add("slideUpReverse");
+			}
+			this.items[id].classList.add("slideSideReverse");
+			for (let i = id + 1; i < this.items.length; i++) {
+				this.items[i].classList.add("slideDownReverse");
+			}
+		});
+	},
+
+	init() {
+		this.items.forEach((el, idx) => {
+			el.addEventListener("animationend", (e) => {
+				if (
+					e.currentTarget.classList.contains("slideSide") ||
+					e.currentTarget.classList.contains("slideDown") ||
+					e.currentTarget.classList.contains("slideUp")
+				) {
+					e.currentTarget.classList.add("display-none");
+					this.scene.classList.add("display-none");
+				}
+				e.currentTarget.classList.remove(
+					"slideSide",
+					"slideUp",
+					"slideDown",
+					"slideSideReverse",
+					"slideUpReverse",
+					"slideDownReverse",
+					"disabled",
+				);
+			});
+			el.addEventListener("pointerdown", () => {
+				this.disableAllItems();
+				MainContentSystem.revealMainContent(this.items[idx]);
+				this.hideMenu(idx);
+				let idxy = idx + 1;
+				NavbarSystem.setActiveNavMenuItem(idxy);
+				NavbarSystem.showNavbar();
+			});
+			el.addEventListener("touchstart", (ev) => {
+				ev.preventDefault();
+				this.disableAllItems();
+				MainContentSystem.revealMainContent(this.items[idx]);
+				this.hideMenu(idx);
+				let idxy = idx + 1;
+				NavbarSystem.setActiveNavMenuItem(idxy);
+				NavbarSystem.showNavbar();
 			});
 		});
-	} else {
-		el.addEventListener("click", () => {
-			mainContentArray.forEach((section) => {
-				section.classList.add("display-none");
-			});
-			navMenuItemsArray.forEach((highlight) => {
-				highlight.classList.remove("active");
-			});
-			navMenuItemsArray[idx].classList.add("active");
-			reveal(el);
-			closeSideMenu();
-			hamburger_button.classList.add("expanded");
+	},
+};
+
+const MainContentSystem = {
+	mainContentArray: document.querySelectorAll(".main-container > div"),
+
+	revealMainContent(ev) {
+		if (!ev) return;
+		const target = ev.getAttribute("data-target");
+		document.getElementById(target).classList.remove("display-none");
+		document.getElementById(target).classList.add("animation-fade-in");
+	},
+	hideMainContent() {
+		this.mainContentArray.forEach((section) => {
+			section.classList.add("display-none");
 		});
-	}
-});
+	},
+
+	init() {
+		this.mainContentArray.forEach((section) => {
+			section.addEventListener("animationend", () => {
+				section.classList.remove("animation-fade-in");
+			});
+		});
+	},
+};
+
+const NavbarSystem = {
+	navMenu: document.querySelector(".nav-menu-container"),
+	navItems: document.querySelector(".nav-menu").querySelectorAll(".nav-menu-item"),
+	title: document.querySelector(".title-text"),
+	overlay: document.querySelector(".nav-menu-overlay"),
+	hamburgerButton: document.querySelector(".hamburger-button"),
+
+	setActiveNavMenuItem(i) {
+		this.navItems.forEach((e) => {
+			e.classList.remove("active");
+			e.classList.remove("disabled");
+		});
+		this.navItems[i].classList.add("active");
+	},
+	showNavbar() {
+		this.navMenu.classList.remove("display-none");
+		this.title.classList.add("faded");
+		this.hamburgerButton.classList.add("expanded");
+	},
+	closeSideMenu() {
+		this.overlay.classList.remove("nav-menu-overlay-active");
+		this.navMenu.classList.add("nav-menu-hidden");
+	},
+	openSideMenu() {
+		this.overlay.classList.add("nav-menu-overlay-active");
+		this.navMenu.classList.remove("nav-menu-hidden");
+	},
+
+	init() {
+		this.navMenu.addEventListener("animationend", () => {
+			this.navMenu.classList.remove("animation-fade-in");
+			this.navMenu.classList.remove("animation-fade-out");
+		});
+		this.overlay.addEventListener("pointerdown", () =>
+			this.closeSideMenu(),
+		);
+		this.hamburgerButton.addEventListener("pointerdown", () =>
+			this.openSideMenu(),
+		);
+		this.navItems.forEach((el, idx) => {
+			if (idx === 0) {
+				el.addEventListener("pointerdown", () => {
+					this.title.classList.remove("faded");
+					MenuSystem.showMenu();
+					this.closeSideMenu();
+					this.hamburgerButton.classList.remove("expanded");
+					this.navItems.forEach((e) => {
+						e.classList.add("disabled");
+					});
+					this.navMenu.classList.add("animation-fade-out");
+					this.navMenu.addEventListener(
+						"animationend",
+						() => {
+							this.navMenu.classList.add("display-none");
+						},
+						{ once: true },
+					);
+					MainContentSystem.hideMainContent();
+				});
+			} else {
+				el.addEventListener("pointerdown", () => {
+					MainContentSystem.hideMainContent();
+					MainContentSystem.revealMainContent(el);
+					this.setActiveNavMenuItem(idx);
+					this.closeSideMenu();
+				});
+			}
+		});
+	},
+};
+
+MenuSystem.init();
+MainContentSystem.init();
+NavbarSystem.init();
